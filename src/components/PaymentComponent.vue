08f40895e22a31183aa39d2c7fdf85b4ac5e4cc9
Hyun Jeong Shin <hj0304hj@gmail.com>
2023-05-24 06:41:38 +0900

    카드 간편 결제 테이블에 저장

--- src/components/PaymentComponent.vue
+++ src/components/PaymentComponent.vue
@@ -47,6 +47,7 @@ export default {
             address : {},
             message : "",
             merchantUid: "", // 주문번호
+            impUid: "", // 결제번호
             
             type : "",
             contract : {}, 
@@ -91,6 +92,11 @@ export default {
             const timestamp = Date.now();
             state.merchantUid = `${timestamp}_${state.memberNumber}`;
         }
+        // 카드 간편 결제시 결제번호 생성
+        const setImpUid = () => {
+            const timestamp = Math.floor(Date.now()/100).toString(); // 11자리 맞추기
+            state.ImpUid = `imp_${timestamp}${state.memberNumber}`;
+        }
 
         // 빠른배송 구매 and 즉시구매->거래체결 테이블에 저장, 보관판매->거래체결 테이블에 저장x
         const contractDto = state.type === 'keep' ? null : state.contract;
@@ -117,7 +123,7 @@ export default {
         const IMP = window.IMP; 
         IMP.init("imp26282104"); // 상점id
 
-        const requestPay = () => {
+        const requestPay = async() => {
             if(!state.address.address) {
                 alert('주소를 입력하세요.')
                 return false
@@ -127,126 +133,199 @@ export default {
                 return false
             }
             if(state.type === 'fast' || state.type === 'normal') {// 구매-빠른배송, 즉시구매
-                IMP.request_pay(
-                    {
-                        pg: 'kakaopay',
-                        pay_method: 'card',
-                        merchant_uid: state.merchantUid,
-                        name: state.item.productKorName, // 상품명
-                        amount: state.finalPrice, // 결제가격
-                        buyer_email: state.member.email, 
-                        buyer_name: state.member.name,  
-                        buyer_tel: state.member.phoneNumber, 
-                        buyer_addr: state.address.address + state.address.subAddress,
-                        buyer_postcode: state.address.zipCode,
-                    },
-
-                    async (rsp) => {
-                        console.log("알에스피", rsp);
-                        if (rsp.success) {
-                            const url = `/api/add/payment?type=${state.type}`;
-                            const headers = { "Content-Type": "application/json" };
-                            const data = {
-                                contractDto,
-                                sellingDto,
-                                paymentDto : {        
-                                    impUid: rsp.imp_uid, // 결제번호
-                                    merchantUid: rsp.merchant_uid, // 주문번호
-                                    contractId : null,
-                                    sellingId : state.item.sellingId,
-                                    memberNumber : state.memberNumber,
-                                    name : state.member.name, 
-                                    address : state.address.address,
-                                    subAddress : state.address.subAddress,
-                                    zipCode : state.address.zipCode,
-                                    phoneNumber : state.address.phoneNumber, 
-                                    message : state.message, 
-                                    paymentType : state.paymentType,
-                                    price : state.finalPrice
-                                }
+                if(state.paymentType === 1) { // 카드 간편 결제
+                    if(confirm('확인을 누르면 선택하신 카드로 즉시 결제됩니다.')) {
+                        const url = `/api/add/payment?type=${state.type}`;
+                        const headers = { "Content-Type": "application/json" };
+                        const data = {
+                            contractDto,
+                            sellingDto,
+                            paymentDto : {        
+                                impUid: state.ImpUid, // 결제번호
+                                merchantUid: state.merchantUid, // 주문번호
+                                contractId : null,
+                                sellingId : state.item.sellingId,
+                                memberNumber : state.memberNumber,
+                                name : state.member.name, 
+                                address : state.address.address,
+                                subAddress : state.address.subAddress,
+                                zipCode : state.address.zipCode,
+                                phoneNumber : state.address.phoneNumber, 
+                                message : state.message, 
+                                paymentType : state.paymentType,
+                                price : state.finalPrice
                             }
-                            console.log("바디",data);
-                            const res = await axios.post(url, data, {headers});
-                            console.log("결제 완료", res);
-                            if(res.status === 200) {
-                                router.push({
-                                    path : '/buying/complete',
-                                    query : {
-                                        productid: state.item.id,
-                                        type : state.type
-                                    }
-                                })
-                            }
-                            
-                        } else {
-                            alert(`결제에 실패하였습니다. 에러 내용: ${rsp.error_msg}`);
                         }
-                    } 
-                )
+                        console.log("간편결제 바디",data);
+                        const res = await axios.post(url, data, {headers});
+                        console.log("간편결제 완료", res);
+                        if(res.status === 200) {
+                            router.push({
+                                path : '/buying/complete',
+                                query : {
+                                    productid: state.item.id,
+                                    type : state.type
+                                }
+                            })
+                        }
+                    }
+                } else { // 카카오 페이
+                    IMP.request_pay(
+                        {
+                            pg: 'kakaopay',
+                            pay_method: 'card',
+                            merchant_uid: state.merchantUid,
+                            name: state.item.productKorName, // 상품명
+                            amount: state.finalPrice, // 결제가격
+                            buyer_email: state.member.email, 
+                            buyer_name: state.member.name,  
+                            buyer_tel: state.member.phoneNumber, 
+                            buyer_addr: state.address.address + state.address.subAddress,
+                            buyer_postcode: state.address.zipCode,
+                        },
+
+                        async (rsp) => {
+                            console.log("알에스피", rsp);
+                            if (rsp.success) {
+                                const url = `/api/add/payment?type=${state.type}`;
+                                const headers = { "Content-Type": "application/json" };
+                                const data = {
+                                    contractDto,
+                                    sellingDto,
+                                    paymentDto : {        
+                                        impUid: rsp.imp_uid, // 결제번호
+                                        merchantUid: rsp.merchant_uid, // 주문번호
+                                        contractId : null,
+                                        sellingId : state.item.sellingId,
+                                        memberNumber : state.memberNumber,
+                                        name : state.member.name, 
+                                        address : state.address.address,
+                                        subAddress : state.address.subAddress,
+                                        zipCode : state.address.zipCode,
+                                        phoneNumber : state.address.phoneNumber, 
+                                        message : state.message, 
+                                        paymentType : state.paymentType,
+                                        price : state.finalPrice
+                                    }
+                                }
+                                console.log("카카오 바디",data);
+                                const res = await axios.post(url, data, {headers});
+                                console.log("카카오 결제 완료", res);
+                                if(res.status === 200) {
+                                    router.push({
+                                        path : '/buying/complete',
+                                        query : {
+                                            productid: state.item.id,
+                                            type : state.type
+                                        }
+                                    })
+                                }
+                            } else {
+                                alert(`결제에 실패하였습니다. 에러 내용: ${rsp.error_msg}`);
+                            }
+                        } 
+                    )
+                }
             } else if (state.type ==='keep') { // 판매-보관판매
                 if(!state.member.depositor) {
                     alert('판매 정산 계좌를 입력하세요.')
                     return false
                 }
-                IMP.request_pay(
-                    {
-                        pg: 'kakaopay',
-                        pay_method: 'card',
-                        merchant_uid: state.merchantUid,
-                        name: '창고 이용료', // 상품명
-                        amount: 3000, // 창고이용료 
-                        buyer_email: state.member.email, 
-                        buyer_name: state.member.name,  
-                        buyer_tel: state.member.phoneNumber, 
-                        buyer_addr: state.address.address + state.address.subAddress,
-                        buyer_postcode: state.address.zipCode,
-                    },
-
-                    async (rsp) => {
-                        console.log("알에스피", rsp);
-                        if (rsp.success) {
-                            const url = `/api/add/payment?type=${state.type}`;
-                            const headers = { "Content-Type": "application/json" };
-                            const data = {
-                                contractDto,
-                                sellingDto,
-                                paymentDto : {        
-                                    impUid: rsp.imp_uid, // 결제번호
-                                    merchantUid: rsp.merchant_uid, // 주문번호
-                                    contractId : null,
-                                    sellingId : null,
-                                    memberNumber : state.memberNumber,
-                                    name : state.member.name, 
-                                    address : state.address.address,
-                                    subAddress : state.address.subAddress,
-                                    zipCode : state.address.zipCode,
-                                    phoneNumber : state.address.phoneNumber, 
-                                    message : state.message, 
-                                    paymentType : state.paymentType,
-                                    price : 3000
-                                }
+                if(state.paymentType === 1) { // 카드 간편 결제
+                    if(confirm('확인을 누르면 선택하신 카드로 즉시 결제됩니다.')) {
+                        const url = `/api/add/payment?type=${state.type}`;
+                        const headers = { "Content-Type": "application/json" };
+                        const data = {
+                            contractDto,
+                            sellingDto,
+                            paymentDto : {        
+                                impUid: state.ImpUid, // 결제번호
+                                merchantUid: state.merchantUid, // 주문번호
+                                contractId : null,
+                                sellingId : null,
+                                memberNumber : state.memberNumber,
+                                name : state.member.name, 
+                                address : state.address.address,
+                                subAddress : state.address.subAddress,
+                                zipCode : state.address.zipCode,
+                                phoneNumber : state.address.phoneNumber, 
+                                message : state.message, 
+                                paymentType : state.paymentType,
+                                price : 3000
                             }
-                            console.log("바디",data);
-                            const res = await axios.post(url, data, {headers});
-                            console.log("결제 완료", res);
-                            if(res.status === 200) {
-                                router.push({
-                                    path : '/selling/complete',
-                                    query : {
-                                        productid: state.selling.productId,
-                                        type : state.type
-                                    }
-                                })
-                            }
-                            
-                        } else {
-                            alert(`결제에 실패하였습니다. 에러 내용: ${rsp.error_msg}`);
                         }
-                    } 
-                )
+                        console.log("간편결제 바디",data);
+                        const res = await axios.post(url, data, {headers});
+                        console.log("간편 결제 완료", res);
+                        if(res.status === 200) {
+                            router.push({
+                                path : '/selling/complete',
+                                query : {
+                                    productid: state.selling.productId,
+                                    type : state.type
+                                }
+                            })
+                        }
+                    }
+                } else { // 카카오 페이
+                    IMP.request_pay(
+                        {
+                            pg: 'kakaopay',
+                            pay_method: 'card',
+                            merchant_uid: state.merchantUid,
+                            name: '창고 이용료', // 상품명
+                            amount: 3000, // 창고이용료 
+                            buyer_email: state.member.email, 
+                            buyer_name: state.member.name,  
+                            buyer_tel: state.member.phoneNumber, 
+                            buyer_addr: state.address.address + state.address.subAddress,
+                            buyer_postcode: state.address.zipCode,
+                        },
+
+                        async (rsp) => {
+                            console.log("알에스피", rsp);
+                            if (rsp.success) {
+                                const url = `/api/add/payment?type=${state.type}`;
+                                const headers = { "Content-Type": "application/json" };
+                                const data = {
+                                    contractDto,
+                                    sellingDto,
+                                    paymentDto : {        
+                                        impUid: rsp.imp_uid, // 결제번호
+                                        merchantUid: rsp.merchant_uid, // 주문번호
+                                        contractId : null,
+                                        sellingId : null,
+                                        memberNumber : state.memberNumber,
+                                        name : state.member.name, 
+                                        address : state.address.address,
+                                        subAddress : state.address.subAddress,
+                                        zipCode : state.address.zipCode,
+                                        phoneNumber : state.address.phoneNumber, 
+                                        message : state.message, 
+                                        paymentType : state.paymentType,
+                                        price : 3000
+                                    }
+                                }
+                                console.log("카카오 바디",data);
+                                const res = await axios.post(url, data, {headers});
+                                console.log("카카오 결제 완료", res);
+                                if(res.status === 200) {
+                                    router.push({
+                                        path : '/selling/complete',
+                                        query : {
+                                            productid: state.selling.productId,
+                                            type : state.type
+                                        }
+                                    })
+                                }
+                            } else {
+                                alert(`결제에 실패하였습니다. 에러 내용: ${rsp.error_msg}`);
+                            }
+                        } 
+                    )
+                }
             }
         }
-
 
         onMounted(()=>{
             if(props.showPayment){
@@ -254,6 +333,7 @@ export default {
             }
             handleData();
             setMerchantUid();
+            setImpUid();
         })
 
         return {
